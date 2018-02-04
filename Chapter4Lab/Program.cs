f938557ab73ebc8247135f7f05d22f598b33b934
LSLIM <LSLIM@LS>
2018-02-04 18:06:12 +0100

    I made a several changes after I implemented Huib syntax

--- Chapter4Lab/Program.cs
+++ Chapter4Lab/Program.cs
@@ -14,57 +14,92 @@ namespace Chapter4Lab
             // Perform the following operations on them:
             // 1. Using a ternary operator, print who is older between two people.
             // 2. Using comparison and logical operators, evaluate and print who is the oldest and the youngest in the group of three.
-            var henkAge = 14;
-            var janAge = 37;
-            var keesAge = 66;
-            if ((henkAge > janAge) && (henkAge > keesAge))
+            Person person1 = new Person();
+            Person person2 = new Person();
+            Person person3 = new Person();
+
+            person1.Age = 14;
+            person1.Name = "Henk";
+
+            person2.Age = 37;
+            person2.Name = "Jan";
+
+            person3.Age = 66;
+            person3.Name = "Kees";
+
+            if (Person.Older(person2.Age, person1.Age))
             {
-                Console.WriteLine("The oldest of the three is henkAge");
+                Console.WriteLine(person2.Name + " is ouder dan " + person1.Name);
             }
-            else if ((janAge > henkAge) && (janAge > keesAge))
+            else
             {
-                Console.WriteLine("The oldest of the three is janAge");
+                Console.WriteLine(person2.Name + " is jonger dan of even oud als " + person1.Name);
             }
-            else if ((keesAge > henkAge) && (keesAge > janAge))
+
+            if (Person.Older(person2.Age, person3.Age))
             {
-                Console.WriteLine("The oldest of the three is keesAge");
+                Console.WriteLine(person2.Name + " is ouder dan " + person3.Name);
             }
-            Youngest();
-            Ternary();
+            else
+            {
+                Console.WriteLine(person2.Name + " is jonger dan of even oud als " + person3.Name);
+            }
             Console.ReadLine();
         }
-        static void Youngest()
-        {
-            var henkAge = 14;
-            var janAge = 37;
-            var keesAge = 66;
-            if ((henkAge < janAge) && (henkAge < keesAge))
-            {
-                Console.WriteLine("The youngest of the three is henkAge");
-            }
-            else if ((janAge < henkAge) && (janAge < keesAge))
-            {
-                Console.WriteLine("The youngest of the three is janAge");
-            }
-            else if ((keesAge < henkAge) && (keesAge < janAge))
-            {
-                Console.WriteLine("The youngest of the three is keesAge");
-            }
         }
-        static void Ternary()
+        public class Person
         {
-            var henkAge = 14;
-            var janAge = 37;
-            var keesAge = 66;
-            var result = henkAge > janAge ? "The older of the two is henkAge" : "The older of the two is janAge";
-            var result1 = henkAge > keesAge ? "The older of the two is henkAge" : "The older of the two is keesAge";
-            var result2 = janAge > henkAge ? "The older of the two is janAge" : "The older of the two is henkAge";
-            var result3 = janAge > keesAge ? "The older of the two is janAge" : "The older of the two is keesAge";
-            var result4 = keesAge > henkAge ? "The older of the two is keesAge" : "The older of the two is henkAge";
-            var result5 = keesAge > janAge ? "The older of the two is keesAge" : "The older of the two is janAge";
-            {
-                Console.WriteLine(result + "\n" + result1 + "\n" + result2 + "\n" + result3 + "\n" + result4 + "\n" + result5);
+            int _age;
+            string _name;
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public static bool Older(int age1, int age2)
+        {
+            return (age1 > age2) ? true : false; // ternary operator
+        }
+
+        public static bool Oldest(int age1, int age2, int age3)
+        {
+            if ((age1 > age2) && (age1 > age3))
+                {
+                    Console.WriteLine("The oldest of the three is {0}", age1);
+                }
+            else if ((age2 > age1) && (age2 > age3))
+                {
+                    Console.WriteLine("The oldest of the three is {1}", age2);
+                }
+            else if ((age3 > age1) && (age3 > age2))
+                {
+                    Console.WriteLine("The oldest of the three is {3}", age3);
+                }
+            }
+
+        public static bool Youngest(int age1, int age2, int age3)
+        {
+             if ((age1 > age2) && (age1 > age3))
+                {
+                    Console.WriteLine("The youngest of the three is {4} ", age1);
+                }
+             else if ((age2 > age1) && (age2 > age3))
+                {
+                    Console.WriteLine("The youngest of the three is {5}", age2);
+                }
+             else if ((age3 > age1) && (age3 > age2))
+                {
+                    Console.WriteLine("The youngest of the three is {6}", age3);
+                }
             }
         }
     }
-}
+
